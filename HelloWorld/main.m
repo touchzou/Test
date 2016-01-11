626eb38072ce1fbecd84fbf1a9d7fc665adbb947
emperorjade <emperorjade@emperorjades-MacBook-Air.local>
2016-01-11 09:59:52 +0800

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by emperorjade on 16/1/11.
+//  Copyright © 2016年 emperorjade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
